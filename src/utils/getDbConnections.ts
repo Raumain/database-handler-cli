642d4634fdc195cbed8bdf60fee7cba9e13e03ea
Raumain <romaintibo6@gmail.com>
2025-11-03 19:58:46 +0100

    feat: allow db urls to only start and not be equal to DATABASE_URL

--- src/utils/getDbConnections.ts
+++ src/utils/getDbConnections.ts
@@ -12,11 +12,16 @@ export function getDbConnectionsFromEnv(): Map<string, string> {
 
 		if (trimmedLine.startsWith("#")) {
 			currentComment = trimmedLine.slice(1).trim();
-		} else if (trimmedLine.startsWith("DATABASE_URL")) {
+		} else if (/^DATABASE_URL[_=]/.test(trimmedLine)) {
 			const [key, value] = trimmedLine.split("=").map((part) => part.trim());
-			dbConnections.set(currentComment || key, value);
+			let identifier = currentComment;
+			if (currentComment === "") {
+				identifier =
+					key.split("_").slice(2).join("_").toLowerCase() || "unknown";
+			}
+			dbConnections.set(identifier, value);
+			currentComment = "";
 		}
 	}
-
 	return dbConnections;
 }
